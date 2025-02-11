f80fd11a7e574249a036ceb7b193a815cda8fddb
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2025-02-11 17:55:27 +0100

    initial commit and built the initial components of the projects

---
+++ gpt3_web/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+// This renders the App component into the root div from the HTML
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
