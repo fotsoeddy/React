a094f2ceedae172fc02ac562308427b093bbcd39
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2025-02-13 07:30:14 +0100

    starting the what is chatgpt

--- gpt3_web/src/containers/whatGPT3/WhatGPT3.jsx
+++ gpt3_web/src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import './whatGPT3.css'
+import './whatGPT3.css';
+import { Feature } from '../../components';
+
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature />
+      </div>
+      <div className='gpt3__whatgpt3-heading'></div>
+    </div>
   )
 }
 
