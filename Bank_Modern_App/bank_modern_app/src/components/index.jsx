50a3f327c058989c8191c283e74271573b457062
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2025-02-14 15:28:54 +0100

    Creation of the components

---
+++ Bank_Modern_App/bank_modern_app/src/components/index.jsx
@@ -0,0 +1,25 @@
+import Navbar from './Navbar'
+import Hero from './Hero'
+import Stats from './Stats'
+import Bussiness from './Bussiness'
+import Billing from './Billing'
+import CardDeal from './CardDeal'
+import Testimonials from './Testimonials'
+import Clients from './Clients'
+import CTA from './CTA'
+import Footer from './Footer'
+import Button from './Button' 
+
+export {
+  Navbar,
+  Hero,
+  Stats,
+  Bussiness,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+  Button,
+}
