982eb689471b0d82819b42dd8e613f2ce6101796
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2025-02-14 15:32:26 +0100

    Creation of some short cut for styling

--- Bank_Modern_App/bank_modern_app/src/style.jsx
+++ Bank_Modern_App/bank_modern_app/src/style.jsx
@@ -12,7 +12,7 @@ const styles = {
     padding: "sm:px-16 px-6 sm:py-12 py-4",
   
     marginX: "sm:mx-16 mx-6",
-    marginY: "sm:my-16 my-6",
+    marginY: "sm:my-16 my-6"
   };
   
   export const layout = {
